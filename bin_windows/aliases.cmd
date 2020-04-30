838f42e78b980c1f1aa94577a9c65cc59cf472c4
Fundor333 <fundor333@fundor333.com>
2020-04-30 10:49:30 +0200

    Add command windows

---
+++ bin_windows/aliases.cmd
@@ -0,0 +1,9 @@
+@echo off
+ 
+DOSKEY ..='cd ..'
+DOSKEY ...='cd ..\..'
+DOSKEY sl='dir'
+DOSKEY ls='dir'
+DOSKEY myip="curl http://ipecho.net/plain; echo"
+DOSKEY start_server='python -m http.server 8888'
+DOSKEY gitconfig='code ~\.gitconfig'
